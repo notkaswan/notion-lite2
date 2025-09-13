64b192e0760d6ccb9d956ee96fa6f2ed1fd1bfdb
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-09-14 05:02:14 +0530

    new file:   .postcssrc 	new file:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	new file:   src/App.js 	deleted:    src/App.tsx 	modified:   src/index.html 	new file:   src/index.js 	deleted:    src/index.tsx 	deleted:    tsconfig.json 	modified:   yarn.lock

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="bg-amber-500">
+      <h1>Parcel React App</h1>
+      <p>
+        Edit <code>src/App.tsx</code> to get started!
+      </p>
+    </div>
+  );
+}
