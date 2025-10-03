d751487bdce8eca5e16c4dad5a27ca8236a32486
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-10-04 03:47:59 +0530

    modified:   src/components/Login.js 	new file:   src/utils/validate.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,128 @@
-import { Link } from "react-router";
+import { useRef, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { checkValidateData } from "../utils/validate";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
+  const [isSignInForm, setISSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    const message = checkValidateData(
+      email.current.value,
+      password.current.value
+    );
+    setErrorMessage(message);
+
+    if (message) return;
+
+    if (!isSignInForm) {
+      //sign in logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+          })
+            .then(() => {
+              // Profile updated!
+              // ...
+              const { uid, email, displayName } = auth.currentUser;
+              dispatch(
+                addUser({ uid: uid, email: email, displayName: displayName })
+              );
+            })
+            .catch((error) => {
+              // An error occurred
+              // ...
+              setErrorMessage(error.message);
+            });
+
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      //sign in logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    }
+  };
+  const toggleForm = () => {
+    setISSignInForm(!isSignInForm);
+  };
   return (
     <div>
-      <Link to="/main">Login</Link>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-full max-w-md z-10 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <p className="text-red-700 font-bold text-lg p-2">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleForm}>
+          {isSignInForm
+            ? "New here? Sign Up Now."
+            : "Already registered? Sign In now."}
+        </p>
+      </form>
     </div>
   );
 };
---
+++ src/utils/validate.js
@@ -0,0 +1,14 @@
+export const checkValidateData = (email, password) => {
+  const isEmailValid = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/.test(
+    email
+  );
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,16}$/.test(
+      password
+    );
+
+  if (!isEmailValid) return "Email ID is not valid.";
+  if (!isPasswordValid) return "Password is not valid.";
+
+  return null;
+};
